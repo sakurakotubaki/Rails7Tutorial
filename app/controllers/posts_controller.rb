4c0c4647165f8e8ba434611ccc2983fa9bcd0936
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-07-12 10:54:11 +0900

    add 投稿機能をviewに表示できた

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,13 +3,17 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-
+  # def indexとは、インスタンス変数@postsにPostクラスの全てのインスタンスを代入することを意味する
+  def index
+    @posts = Post.all
+  end
+  # def createとは、インスタンス変数@postにPostクラスのインスタンスを代入し、saveメソッドを実行することを意味する
   def create
     post = Post.new(post_params)
     post.save
     redirect_to '/top'
   end
-
+  # def showとは、インスタンス変数@postにPostクラスのインスタンスを代入することを意味する
   private
   def post_params
     params.require(:post).permit(:location, :text)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   # ２ページ目のルートを追加
   get 'about' => 'homes#about'
-  # 投稿ページのルートを追加
-  post 'posts' => 'posts#create'
+  resources :posts
 end
